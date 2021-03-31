08220da0608edf5c50228350406ff0e23b9dc624
cbarksdale2321 <55416957+cbarksdale2321@users.noreply.github.com>
2021-03-31 14:52:57 -0500

    Fixed Momentum Issues

--- Source/DreamJump/CharacterMovement.h
+++ Source/DreamJump/CharacterMovement.h
@@ -27,6 +27,9 @@ public:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera)
 		UCameraComponent* FollowCamera;
 
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera)
+		AActor* Actor;
+
 	void MoveForward(float Axis);
 	void MoveRight(float Axis);
 
